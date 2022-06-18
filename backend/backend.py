8bcd0083a6b6ca29de85f66a35a9924866975a96
10054645 <kavitesh.kamboj@thalesgroup.com>
2022-06-18 12:53:57 +0530

    Add front end

--- backend/backend.py
+++ backend/backend.py
@@ -5,5 +5,11 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+# To avoid CORS issue
+@app.after_request
+def add_headers(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+    
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=5000)
